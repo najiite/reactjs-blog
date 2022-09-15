63d5f65c95e46deb404dfad1e43e3ae1b3a967df
Onajite Kelvin <onajiteokeh@gmail.com>
2022-09-15 23:27:18 +0100

    Added Pagination

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -10,11 +10,27 @@ import sanityClient from "../SanityClient"
 
 const Home = () => {
   document.title = 'Ai Blog'
+  const noOfPostPerPage = 2
   const [posts, setPosts] = useState([])
+  const [ref2, setRef2] = useState(1)
+  const [noOfPosts, setnoOfPosts] = useState()
+
+  useEffect(() => {
+      sanityClient
+          .fetch(
+              `count(*[_type == "post"])`
+  ).then((data) => {
+    setnoOfPosts(data);
+    })
+  .catch(console.error);
+
+
+  }, [noOfPosts]);
+  
   useEffect(() => {
 		sanityClient
 			.fetch(
-				`*[_type == "post"] | order(_createdAt desc){
+				`*[_type == "post"] | order(_createdAt desc)[0..1]{
           _id,
           _createdAt,
           title,
@@ -38,9 +54,45 @@ const Home = () => {
           },
         }`
 			)
-			.then((data) => setPosts(data))
+			.then((data) => {setPosts(data);
+        setRef2(ref2+noOfPostPerPage);
+      })
 			.catch(console.error);
 	}, []);
+
+  const loadMore =  async () => {
+    await sanityClient.fetch(
+        `*[_type == "post"] | order(_createdAt desc)[0..${ref2}]{
+        _id,
+        _createdAt,
+        title,
+        slug,
+        mainImage{
+          asset->{
+            _ref,
+            url
+          },
+        },
+        body,
+        author->{
+          name,
+          image{
+            asset->{
+              _ref,
+              url
+              }, 
+            },
+          slug,
+        },
+      }`
+   ).then((data) => {
+      setPosts(data);
+      setRef2(ref2+noOfPostPerPage);
+      })
+  .catch(console.error);
+    
+    
+  }
 
   return (
     <div>
@@ -48,16 +100,21 @@ const Home = () => {
         <h3 className="text-4xl mx-5 lg:mx-10">Latest Posts</h3>
       <div className="lg:flex lg:flex-row">
           <div className="lg:basis-3/4 grid md:grid-cols-2 gap-10 m-5 lg:m-10">
-            
               { posts.map((post) => (
-                <Post key={post._id} post={post} />
-              ))}
+                  <Post key={post._id} post={post} />
+                ))}
+                { noOfPosts === posts.length? (<></>):(
+                  <div className="place-content-center">
+                    <button onClick={()=> loadMore()} className="rounded-lg border border-yellow-500 px-5 py-3 hover:text-white hover:bg-yellow-500">Load older posts</button>
+                  </div>)
+                }
           </div>
 
-        <div className="lg:basis-1/4 m-5 lg:my-10" >
-            <Archive />
-        </div>
-      </div><ScrolltoTop />
+          <div className="lg:basis-1/4 m-5 lg:my-10" >
+              <Archive />
+          </div>
+      </div>
+      <ScrolltoTop />
     </div>
   )
 }
