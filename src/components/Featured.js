bacc41cfa30d1bd2dcec814c6b74b1bb3f9831f4
Onajite Kelvin <onajiteokeh@gmail.com>
2022-09-16 03:37:54 +0100

    featured post width adjustment

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -46,11 +46,11 @@ const Featured = () => {
       <>
       <Link to={`/post/${post.slug.current}`}>
 
-        <div className="grid grid-cols-1 place-content-between bg-slate-500 bg-cover bg-no-repeat hover:bg-blend-overlay text-slate-100 p-2 mx-5 my-5 lg:m-10  w-auto lg:h-screen rounded-lg " style={{backgroundImage:  `url(${post.mainImage.asset.url})` }}>
+        <div className="grid grid-cols-1 place-content-between bg-slate-500 bg-cover bg-no-repeat hover:bg-blend-overlay text-slate-100  w-auto lg:h-screen" style={{backgroundImage:  `url(${post.mainImage.asset.url})` }}>
                 <div className='lg:mx-5'>
                   <p className=" flex items-center p-5 text-yellow-500 font-semibold italic"><FaStar /> &nbsp;Featured</p>
                 
-                  <div className="text-3xl md:text-5xl  lg:text-6xl text-yellow-100 italic px-5 md:leading-loose font-semibold">{post.title}</div>
+                  <div className="text-3xl md:text-5xl  lg:text-6xl text-yellow-100 italic px-5 pb-10 md:leading-loose font-semibold">{post.title}</div>
                 </div>
         </div>
       </Link>
